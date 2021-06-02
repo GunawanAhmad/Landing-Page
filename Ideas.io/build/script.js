50939128fcf9493b0e8a35314f7c414d98cd944c
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2021-06-03 06:17:53 +0700

    add pricing page

--- Ideas.io/build/script.js
+++ Ideas.io/build/script.js
@@ -12,3 +12,20 @@ navList.forEach((li) => li.addEventListener("click", toggleMobileNavbar));
 function toggleMobileNavbar() {
   ul.classList.toggle("transform");
 }
+
+const links = document.querySelectorAll("a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+}
